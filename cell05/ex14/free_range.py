394bb5f4655dbd2cf7cf63bd41ad9c056b05d5b8
Narat Tassawil <natassaw@z2t18c4.42bangkok.com>
2025-03-17 20:04:05 +0700

    cell05ex14

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import sys
+
+parameter = sys.argv[1:]
+result = []
+
+if (len(parameter) == 2):
+    for i in range (int(parameter[-1]) - int(parameter[0]) + 1):
+        result.append(int(parameter[0]) + i)
+    print(result)
+else:
+    print("none")
+    
