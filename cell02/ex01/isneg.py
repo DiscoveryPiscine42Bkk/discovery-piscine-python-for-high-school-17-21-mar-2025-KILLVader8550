68293ece654824e704c23963766cff4d44977953
Narat Tassawil <natassaw@z2t18c4.42bangkok.com>
2025-03-17 13:28:01 +0700

    cell02ex01

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+isneg = float(input()) 
+
+if (isneg < 0):
+    print("This number is negative.")
+elif (isneg > 0):
+    print("This number is positive.")
+else:
+    print("This number is both positive and negative.")
