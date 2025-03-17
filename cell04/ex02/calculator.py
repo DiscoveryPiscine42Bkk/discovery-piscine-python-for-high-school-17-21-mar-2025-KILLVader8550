44094babfdc793668249e1342fc6cb7ea9ffb125
Narat Tassawil <natassaw@z2t18c4.42bangkok.com>
2025-03-17 16:32:36 +0700

    cell04ex02

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+first_num = float(input("Give me the first number: "))
+second_num = float(input("Give me the second number: "))
+print("Thank you")
+print(f"{first_num} + {second_num} = {first_num + second_num}")
+print(f"{first_num} - {second_num} = {first_num - second_num}")
+print(f"{first_num} / {second_num} = {first_num / second_num}")
+print(f"{first_num} * {second_num} = {first_num * second_num}")
