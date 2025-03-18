31e64c565025a6a4c0c4d0f7f73978956aae65d4
Narat Tassawil <natassaw@z2t18c4.42bangkok.com>
2025-03-18 14:01:31 +0700

    cell05ex11

--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -2,5 +2,8 @@
 
 import sys
 
-for i in range (1, len(sys.argv)+1):
-    print(f"{sys.argv[i]}: {len(sys.argv[i])}")
+if (len(sys.argv) < 2):
+    print("none")
+else:
+    for i in range (1, len(sys.argv)+1):
+        print(f"{sys.argv[i]}: {len(sys.argv[i])}")
