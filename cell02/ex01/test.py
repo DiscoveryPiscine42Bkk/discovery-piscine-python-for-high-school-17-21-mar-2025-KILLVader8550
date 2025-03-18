6870f9bff0d370796321241200feba2cfb36016b
Narat Tassawil <natassaw@z2t18c4.42bangkok.com>
2025-03-18 15:48:18 +0700

    wyf

---
+++ cell02/ex01/test.py
@@ -0,0 +1,4 @@
+base10 = int(input("Value: "), base = 16)
+base16 = 0b10010
+print(hex(base10))
+print(base16)
--- cell05/ex11/count_it.py
+++ cell05/ex11/count_it.py
@@ -5,5 +5,6 @@ import sys
 if (len(sys.argv) < 2):
     print("none")
 else:
-    for i in range (1, len(sys.argv)+1):
+    print(f"parameters: {len(sys.argv)-1}")
+    for i in range (1, len(sys.argv)):
         print(f"{sys.argv[i]}: {len(sys.argv[i])}")
